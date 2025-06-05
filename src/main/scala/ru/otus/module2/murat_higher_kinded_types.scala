78c296c502171cee2a152c0bd994f515de9c0c00
Дмитрий Мурат <jamesmuret@gmail.com>
2025-06-05 23:53:43 +0300

    Попытка сделать hkt с tupler

--- src/main/scala/ru/otus/module2/murat_higher_kinded_types.scala
+++ src/main/scala/ru/otus/module2/murat_higher_kinded_types.scala
@@ -2,6 +2,28 @@ package ru.otus.module2
 
 object murat_higher_kinded_types {
 
+  trait Tupler[F[_]] {
+    def tupleF[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)]
+  }
 
+
+  object Tupler {
+
+    implicit val optTupler: Tupler[Option] = new Tupler[Option] {
+      override def tupleF[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = {
+        (fa, fb) match {
+          case (Some(a), Some(b)) => Some((a, b))
+          case _ => None
+        }
+      }
+    }
+
+    // суммонер
+    def apply[F[_]](implicit T: Tupler[F[_]]): Tupler[F[_]] = T
+
+    //метод
+    def tupleF[F[_]: Tupler, A, B](fa: F[A], fb: F[B]): F[(A, B)] = Tupler[F].tupleF(fa,fb)
+
+  }
 
 }
